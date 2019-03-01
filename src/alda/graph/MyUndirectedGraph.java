723673b3b1dcf6a5888fc9d7b2424bdc9d68d856
daan3440 <daan3440@student.su.se>
2019-03-01 17:49:41 +0100

    Skit
    Signed-off-by: daan3440 <daan3440@student.su.se>

--- src/alda/graph/MyUndirectedGraph.java
+++ src/alda/graph/MyUndirectedGraph.java
@@ -26,18 +26,6 @@ class MyNode<T>{
 
 	public T data;
 	public HashMap<MyNode<T>, Integer> neighbourMap = new HashMap<>();
-	//	public LinkedList<MyNode<T>> neighbourList = new LinkedList<>();
-
-	//	public ArrayList<T> getNeighbours() {
-	//		System.out.println("Neighbours!");
-	//		ArrayList<T> sendList = (ArrayList<T>) neighbourMap.keySet().stream().collect(Collectors.toList());
-	//		//			List<T> result = neighbourMap.keySet().stream().collect(Collectors.toList());
-	//
-	//		//	        sendList.addAll(result);
-	//		if(sendList == null)
-	//			System.out.println("NULL");
-	//		return sendList;
-	//	}
 
 	public int hashCode() {
 		return data.toString().hashCode();
@@ -69,6 +57,7 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 
 	@Override
 	public int getNumberOfEdges() {
+
 		return nrEdges;
 	}
 
@@ -94,6 +83,7 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 		//		System.out.println(n1.data + " " + n2.data + "  n1 o n2");
 
 		if (cost <= 0 || !nodes.contains(n1) || !nodes.contains(n2)) {
+			n1.neighbourMap.put(n2, cost);
 			return false;
 		} else {
 			for (MyNode<T> cmpNode : nodes) {
@@ -112,6 +102,7 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 					break;
 				}
 			}
+			++nrEdges;
 			return true;
 		}
 	}
@@ -251,110 +242,177 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 		tempList.remove(tempList.size() - 1);
 	}
 	LinkedList<MyNode<T>> queue = new LinkedList<MyNode<T>>(); 
+	//	LinkedList<T> queue = new LinkedList<T>(); 
 	Set<MyNode<T>> visited = new HashSet<MyNode<T>>(); 
 	List<T> path = new ArrayList<T>(); 
 
+	//	@Override
+	//	public List<T>  breadthFirstSearch( T start, T end){
+	////	Map<String,List<String>> adjacentWords
+	//		Map<MyNode<T>,MyNode<T>> parentNode= new HashMap<MyNode<T>,MyNode<T>>( );
+	//		LinkedList<String> q = new LinkedList<String>( );
+	//		
+	//		MyNode<T> s = new MyNode<T>(start);
+	//		MyNode<T> last = new MyNode<T>(end);
+	//		
+	//		queue.addLast( start);
+	//		while( !queue.isEmpty( ) )
+	//		{
+	//			MyNode<T> current = new MyNode<T>(queue.removeFirst());
+	//			Set<MyNode<T>> adj = s.neighbourMap.keySet();
+	//			if( adj != null ) {
+	//				for( MyNode<T> adjNode : adj ) {
+	//					if( parentNode.get( adjNode ) == null )
+	//					{
+	//						parentNode.put( adjNode, current );
+	//						queue.addLast( adjNode.data );
+	//					}
+	//				}
+	//			}
+	//		}
+	//		parentNode.put( s, null );
+	//		return getChainFromPreviousMap( parentNode, s, last);
+	//		}
+	//// After the shortest path calculation has run, computes the List that
+	//// contains the sequence of word changes to get from first to second.
+	//// Returns null if there is no path.
+	//		private List<T> getChainFromPreviousMap( Map<MyNode<T>, MyNode<T>> parentNode, MyNode<T> s, MyNode<T> last ){
+	//			LinkedList<T> result = null;
+	//		    if( parentNode.get( last ) != null )
+	//		    {
+	//		        result = new LinkedList<T>( );
+	//		        for( MyNode<T> data = last; data != null; data = parentNode.get( data ) )
+	//		            result.addFirst( data.data );
+	//		}
+	//		    return result;
+	//		
+	//}
 	@Override
 	public List<T> breadthFirstSearch(T start, T end) {
 		MyNode<T> s = new MyNode<T>(start);
+		//		queue.clear();
 		queue.addLast(s);
-		path = bfs(start, end);
+		if( !bfs(start, end)) {
+			path.clear();
+		}
 		//			LinkedList<T> bfsList = null;
 		//			for(MyNode<T> adding : visited) {
 		//				if (adding.data != null) {
 		//					bfsList.add(adding.data);
 		//				}
 		//			}
-
+		System.out.println(path.size() + " Path!");
 		return path;
 	}
 
-	private List<T> bfs(T start, T end ) {
-		MyNode<T> s = new MyNode<T>(start);
-		MyNode<T> last = new MyNode<T>(end);
-//		System.out.println(queue.size());
-//		queue.pollFirst();
-//		System.out.println(s.neighbourMap.keySet());
+	private boolean bfs(T start, T end ) { //J
+		//queue J
+		MyNode<T> s = new MyNode<T>(start); //J
+		MyNode<T> last = new MyNode<T>(end); //A
+		//		System.out.println(queue.size());
+		//		queue.pollFirst();
+		//		System.out.println(s.neighbourMap.keySet());
 
-		queue.removeFirst();
-		visited.add(s);
-		System.out.println(path.size() + " Pree");
-		path.add(s.data);
-		System.out.println(path.size() + " Post");
-		if(s.neighbourMap.keySet().isEmpty()) {
-			System.out.println(path.size() + " add to Queue");
-			queue.addAll(s.neighbourMap.keySet());
-			System.out.println(path.size() + " Post add to Queue");
+		path.add(s.data); //J
+		while(!queue.isEmpty()) {
+			MyNode<T> cNode = queue.removeFirst();
+			
+			ArrayList<MyNode<T>>  list = new ArrayList<MyNode<T>> (cNode.neighbourMap.keySet().stream().collect(Collectors.toList()));
+			
+			System.out.println(list.size() + " BIG array");
+			if (cNode.equals(last)) {
+				return true;
 			}
-		else if(!s.equals(last)) {
-			path.remove(path.size()-1);
+			if(!visited.contains(cNode)) {
+				visited.add(s);
+			}
+
+			for (int i = 0; i < list.size(); i++ ) {
+						queue.addLast(list.get(i));
+				System.out.println(queue.size() + " Queue Size");
+				//			if ((boolean) queue.peekFirst().data)
+			}
+			if(!queue.isEmpty()) {
+				bfs(queue.peekFirst().data, end);
+			}
 		}
-		System.out.println(queue.size() + " Efter");
-		if(!queue.isEmpty()) {
-			System.out.println(queue.size() + " Rekursiv");
-			bfs(queue.peekFirst().data, last.data);
-		}
-		
-		return path;	
+		return false;
+		//		System.out.println(path.size() + " Pree");
+		//		System.out.println(path.size() + " Post");
+		//		if(s.neighbourMap.keySet().isEmpty()) {
+		//			System.out.println(path.size() + " add to Queue");
+		//			queue.addAll(s.neighbourMap.keySet());
+		//			System.out.println(path.size() + " Post add to Queue");
+		//		}
+		//		else if(!s.equals(last)) {
+		//			path.remove(path.size()-1);
+		//		}
+		//		System.out.println(queue.size() + " Efter");
+		//		if(!queue.isEmpty()) {
+		//			System.out.println(queue.size() + " Rekursiv");
+		//			bfs(queue.peekFirst().data, last.data);
+		//		}
+		//
+		//		return path;	
 
 
-
-		//		MyNode<T> current = new MyNode<T>(start);
-		//		MyNode<T> last = new MyNode<T>(end);
-		//		//		”Set Node 1 as the start Node
-		//		queue.add(current); 
-		////		System.out.println(current+" current"); 
-		//		while (queue.size() != 0 && !current.equals(last)) 
-		//		{ 
 		//
-		//		//		Add this Node to the Queue
-		//		//		Add this Node to the visited set
-		//		current.visited =true;
-		//		
-		//		visited.add(current);
-		//		if (visited != null)
-		//			System.out.println(visited.size() + " visited"); 
-		//			
-		//		//		If this node is our goal node then return true, else add Node 2 and Node 3 to our Queue
+		//		//		MyNode<T> current = new MyNode<T>(start);
+		//		//		MyNode<T> last = new MyNode<T>(end);
+		//		//		//		”Set Node 1 as the start Node
+		//		//		queue.add(current); 
+		//		////		System.out.println(current+" current"); 
+		//		//		while (queue.size() != 0 && !current.equals(last)) 
+		//		//		{ 
+		//		//
+		//		//		//		Add this Node to the Queue
+		//		//		//		Add this Node to the visited set
+		//		//		current.visited =true;
+		//		//		
+		//		//		visited.add(current);
+		//		//		if (visited != null)
+		//		//			System.out.println(visited.size() + " visited"); 
+		//		//			
+		//		//		//		If this node is our goal node then return true, else add Node 2 and Node 3 to our Queue
+		//		//
+		//		//		//		Check Node 2 and if it isn’t add both Node 4 and Node 5 to our Queue. 
+		//		//		//		Take the next node from our Queue which should be Node 3 and check that.
+		//		//		
+		//		//		//		If Node 3 isn’t our goal node add Node 6 and Node 7 to our Queue.
+		//		//		//		Repeat until goal Node is found.”
+		//		//
+		//		//		// Mark the current node as visited and enqueue it 
+		//		//
+		//		//			// Dequeue a vertex from queue and print it 
+		//		//			current = queue.poll(); 
+		//		//			System.out.println(current+" current"); 
+		//		//
+		//		//			// Get all adjacent vertices of the dequeued vertex s 
+		//		//			// If a adjacent has not been visited, then mark it 
+		//		//			// visited and enqueue it 
+		//		//			ArrayList<T> list = current.getNeighbours();
+		//		//			for (T t : list) {
+		//		//				System.out.println(t+ " toString");
+		//		//			}
+		//		//			Iterator<T> i = current.getNeighbours().listIterator(); 
+		//		//			System.out.println(i+" i"); 
+		//		//			if(i.hasNext()) {
+		//		//				System.out.println(" hasNext"); 
+		//		//				
+		//		//			}
+		//		//			while (i.hasNext()) 
+		//		//			{ 
+		//		//				System.out.println(" WHUKE"); 
+		//		//				T n = i.next();
+		//		//				MyNode<T> tmp = new MyNode<T>(n);
+		//		//				if (!tmp.visited) 
+		//		//				{ 
+		//		//					tmp.visited = true; 
+		//		//					queue.add(tmp); 
+		//		//				} 
+		//		//			} 
+		//		//		} 
 		//
-		//		//		Check Node 2 and if it isn’t add both Node 4 and Node 5 to our Queue. 
-		//		//		Take the next node from our Queue which should be Node 3 and check that.
-		//		
-		//		//		If Node 3 isn’t our goal node add Node 6 and Node 7 to our Queue.
-		//		//		Repeat until goal Node is found.”
-		//
-		//		// Mark the current node as visited and enqueue it 
-		//
-		//			// Dequeue a vertex from queue and print it 
-		//			current = queue.poll(); 
-		//			System.out.println(current+" current"); 
-		//
-		//			// Get all adjacent vertices of the dequeued vertex s 
-		//			// If a adjacent has not been visited, then mark it 
-		//			// visited and enqueue it 
-		//			ArrayList<T> list = current.getNeighbours();
-		//			for (T t : list) {
-		//				System.out.println(t+ " toString");
-		//			}
-		//			Iterator<T> i = current.getNeighbours().listIterator(); 
-		//			System.out.println(i+" i"); 
-		//			if(i.hasNext()) {
-		//				System.out.println(" hasNext"); 
-		//				
-		//			}
-		//			while (i.hasNext()) 
-		//			{ 
-		//				System.out.println(" WHUKE"); 
-		//				T n = i.next();
-		//				MyNode<T> tmp = new MyNode<T>(n);
-		//				if (!tmp.visited) 
-		//				{ 
-		//					tmp.visited = true; 
-		//					queue.add(tmp); 
-		//				} 
-		//			} 
-		//		} 
-
 	}
 	@Override
 	public UndirectedGraph<T> minimumSpanningTree() {
