1c5484cd7d9398278860c124e5db99f8a69991ff
daan3440 <daan3440@student.su.se>
2019-03-02 11:40:48 +0100

    Yelin e boss
    Signed-off-by: daan3440 <daan3440@student.su.se>

--- src/alda/graph/MyUndirectedGraph.java
+++ src/alda/graph/MyUndirectedGraph.java
@@ -1,31 +1,26 @@
 package alda.graph;
-import java.util.ArrayList;
-import java.util.Collection;
 /**
  * ALDA07 - Grafer
  * Daniel Andersson - daan3440
  * @author Daniel Andersson - daan3440  
  **/
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Queue;
 import java.util.Set;
-import java.util.Stack;
-import java.util.stream.Collectors;
 
 class MyNode<T>{
-	boolean visited = false;
+	boolean known = false;
 
 	public MyNode(T data) {
 		this.data = data;
 	}
-
 	public T data;
 	public HashMap<MyNode<T>, Integer> neighbourMap = new HashMap<>();
+	public LinkedList<T> visitedNodes = new LinkedList<T>();
 
 	public int hashCode() {
 		return data.toString().hashCode();
@@ -44,7 +39,6 @@ class MyNode<T>{
 }
 
 public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
-	//	private Map<T, Integer> nodes = new HashMap<T, Integer>();
 
 	int nrNodes;
 	int nrEdges;
@@ -72,37 +66,49 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 			return true;
 		}
 	}
-
+	
 	@Override
 	public boolean connect(T node1, T node2, int cost) {
-		MyNode<T> n1 = new MyNode<T>(node1);
-		MyNode<T> n2 = new MyNode<T>(node2);
-		if(cost<0 && n1.equals(n2)) {
-			n1.neighbourMap.put(n2, cost);
+		
+		MyNode<T> n1 = null;
+		MyNode<T> n2 = null;
+
+//		nodes.forEach(cmpN -> {
+//			if (cmpN.data.equals(node2)) { 
+//				n2 = cmpN;
+//				}
+//						});
+//		nodes.forEach(cmpN -> {
+//			if (cmpN.data.equals(node1)) { 
+//				n1 = cmpN;
+//				}
+//		});
+		
+	
+		
+		
+		for (MyNode<T> cmpN : nodes) {
+
+			if (cmpN.data.equals(node2)) {
+				// cmpN.neighbourMap.put(n2, cost);
+				n2 = cmpN;
+				break;
+			}
 		}
-		//		System.out.println(n1.data + " " + n2.data + "  n1 o n2");
+		for (MyNode<T> cmpN : nodes) {
+			if (cmpN.data.equals(node1)) {
+				n1 = cmpN;
+				// cmpN.neighbourMap.put(n2, cost);
+				break;
+			}
+		}
 
 		if (cost <= 0 || !nodes.contains(n1) || !nodes.contains(n2)) {
-			n1.neighbourMap.put(n2, cost);
 			return false;
 		} else {
-			for (MyNode<T> cmpNode : nodes) {
-				if (cmpNode.equals(n1)) {
-					//					System.out.println(" n1 found i connect");
-					cmpNode.neighbourMap.put(n2, cost);
-					//					System.out.println(cmpNode.neighbourMap.size() + " nMap Size");
-					break;
-				}
-			}
-			for (MyNode<T> cmpNode : nodes) {
-				if (cmpNode.equals(n2)) {
-					//					System.out.println(" n2 found i connect");
-					cmpNode.neighbourMap.put(n1, cost);
-					//					System.out.println(cmpNode.neighbourMap.size() + " nMap Size 2");
-					break;
-				}
-			}
-			++nrEdges;
+			n1.neighbourMap.put(n2, cost);
+			n2.neighbourMap.put(n1, cost);
+			// förbättring på koden?
 			return true;
 		}
 	}
@@ -141,13 +147,55 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 
 	//Depth first
 	private List<T> depthList = new LinkedList<>();
-	private List<T> tempList = new ArrayList<>();
+	private List<T> stack = new ArrayList<>();
 
 	//	private List<T> visited = new LinkedList<T>();
-	private Stack<T> stack = new Stack<T>();
+	//	private Stack<T> stack = new Stack<T>();
+	//	@Override
+	//	public List<T> depthFirstSearch(T start, T end) {
+	//		dfs(start, end);
+	//		return depthList;
+	//	}	
 	@Override
 	public List<T> depthFirstSearch(T start, T end) {
-		dfs(start, end);
+		MyNode<T> current= new MyNode<T>(start);
+		MyNode<T> goal = new MyNode<T>(end);
+		MyNode<T> tmp = null;
+
+		for (MyNode<T> cmpN : nodes) {
+			if (cmpN.data.equals(current.data)) {
+				tmp = cmpN;
+				break;
+			}
+		}
+		if (tmp == null) {
+			return stack;
+		}
+		Set<MyNode<T>> set = tmp.neighbourMap.keySet();
+
+		stack.add(current.data);
+		if (current.data.equals(goal.data)) {
+			if (depthList.isEmpty()) {
+				depthList.addAll(stack);
+			}
+			if (stack.size() < depthList.size()) { //From  A to J, Same 2 Same
+				depthList.clear();
+				depthList.addAll(stack);
+			}
+			stack.remove(stack.size() - 1);
+			return depthList;
+		}
+		// if the end-node is not found in this round,go to next round
+		for (MyNode<T> nextNode : set) {
+			// TODO push the subNode
+			if (!nextNode.known) {
+				nextNode.known= true;
+				depthFirstSearch(nextNode.data, goal.data);
+				nextNode.known = false;
+			}
+		}
+		// TODO pop the parentNode
+		stack.remove(stack.size() - 1);
 		return depthList;
 	}
 	//	@Override
@@ -199,49 +247,50 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 	//		return stack;//dfs(start, end);;
 	//	}
 
-	private void dfs(T in, T exit) {
-		MyNode<T> current= new MyNode<T>(in);
-		MyNode<T> end = new MyNode<T>(exit);
-		MyNode<T> tmp = null;
-
-		for (MyNode<T> tmpNode : nodes) {
-			if (tmpNode.data.equals(current.data)) {
-				tmp = tmpNode;
-				//				System.out.println(tmp + "");
-				break;
-			}
-		}
-		if (tmp == null) {
-			return;
-		}
-		Set<MyNode<T>> set = tmp.neighbourMap.keySet();
-
-		tempList.add(current.data);
-		if (current.data.equals(end.data)) {
-			if (depthList.isEmpty()) {
-				depthList.addAll(tempList);
-			} else {
-				if (tempList.size() < depthList.size()) {
-					depthList.clear();
-					depthList.addAll(tempList);
-				}
-			}
-			tempList.remove(tempList.size() - 1);
-			return;
-		}
-		// if the end-node is not found in this round,go to next round
-		for (MyNode<T> nextNode : set) {
-			// TODO push the subNode
-			if (!nextNode.visited) {
-				nextNode.visited= true;
-				dfs(nextNode.data, end.data);
-				nextNode.visited = false;
-			}
-		}
-		// TODO pop the parentNode
-		tempList.remove(tempList.size() - 1);
-	}
+	//	private void dfs(T in, T exit) {
+	//		MyNode<T> current= new MyNode<T>(in);
+	//		MyNode<T> end = new MyNode<T>(exit);
+	//		MyNode<T> tmp = null;
+	//
+	//		for (MyNode<T> tmpNode : nodes) {
+	//			if (tmpNode.data.equals(current.data)) {
+	//				tmp = tmpNode;
+	//				//				System.out.println(tmp + "");
+	//				break;
+	//			}
+	//		}
+	//		if (tmp == null) {
+	//			return;
+	//		}
+	//		Set<MyNode<T>> set = tmp.neighbourMap.keySet();
+	//
+	//		tempList.add(current.data);
+	//		if (current.data.equals(end.data)) {
+	//			if (depthList.isEmpty()) {
+	//				depthList.addAll(tempList);
+	//			} else {
+	//				if (tempList.size() < depthList.size()) {
+	//					depthList.clear();
+	//					depthList.addAll(tempList);
+	//				}
+	//			}
+	//			tempList.remove(tempList.size() - 1);
+	//			return;
+	//		}
+	//		// if the end-node is not found in this round,go to next round
+	//		for (MyNode<T> nextNode : set) {
+	//			// TODO push the subNode
+	//			if (!nextNode.known) {
+	//				nextNode.known= true;
+	//				dfs(nextNode.data, end.data);
+	//				nextNode.known = false;
+	//			}
+	//		}
+	//		// TODO pop the parentNode
+	//		tempList.remove(tempList.size() - 1);
+	//	}
 	LinkedList<MyNode<T>> queue = new LinkedList<MyNode<T>>(); 
+
 	//	LinkedList<T> queue = new LinkedList<T>(); 
 	Set<MyNode<T>> visited = new HashSet<MyNode<T>>(); 
 	List<T> path = new ArrayList<T>(); 
@@ -289,26 +338,65 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 	//}
 	@Override
 	public List<T> breadthFirstSearch(T start, T end) {
-		MyNode<T> s = new MyNode<T>(start);
-		//		queue.clear();
-		queue.addLast(s);
-		if( !bfs(start, end)) {
-			path.clear();
+		Queue<MyNode<T>> queue = new LinkedList<>();
+		MyNode<T> current = null;
+		for (MyNode<T> cmpN : nodes) {
+			if (cmpN.data.equals(start)) {
+				current = cmpN;
+				break;
+			}
 		}
-		//			LinkedList<T> bfsList = null;
-		//			for(MyNode<T> adding : visited) {
-		//				if (adding.data != null) {
-		//					bfsList.add(adding.data);
-		//				}
-		//			}
-		System.out.println(path.size() + " Path!");
-		return path;
+		queue.add(current);
+		current.known = true;
+
+		while (!queue.isEmpty() && !current.data.equals(end)) {
+			MyNode<T> n = queue.poll();		
+			n.visitedNodes.add(n.data);
+
+			for (MyNode<T> mn2 : n.neighbourMap.keySet()) {
+				if (!mn2.known) {
+					mn2.visitedNodes.addAll(n.visitedNodes);
+					queue.add(mn2);
+					mn2.known = true;
+				}
+			}
+			current=queue.peek();			
+		}
+		if (current.data.equals(end)) {
+			current.visitedNodes.add(current.data);// 加他自己
+			return current.visitedNodes;
+		}
+		return null;
 	}
+	//	@Override
+	//	public List<T> breadthFirstSearch(T start, T end) {
+	//		MyNode<T> s = new MyNode<T>(start);
+	//		//		queue.clear();
+	//		queue.addLast(s);
+	//		if( !bfs(start, end)) {
+	//			path.clear();
+	//		}
+	//		//			LinkedList<T> bfsList = null;
+	//		//			for(MyNode<T> adding : visited) {
+	//		//				if (adding.data != null) {
+	//		//					bfsList.add(adding.data);
+	//		//				}
+	//		//			}
+	//		System.out.println(path.size() + " Path!");
+	//		return path;
+	//	}
 
 	private boolean bfs(T start, T end ) { //J
 		//queue J
-		MyNode<T> s = new MyNode<T>(start); //J
+		MyNode<T> s = null;// new MyNode<T>(start); //J
 		MyNode<T> last = new MyNode<T>(end); //A
+
+		for (MyNode<T> mn : nodes) {
+			if (mn.data.equals(start)) {
+				s = mn;
+				break;
+			}
+		}
 		//		System.out.println(queue.size());
 		//		queue.pollFirst();
 		//		System.out.println(s.neighbourMap.keySet());
@@ -316,9 +404,9 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 		path.add(s.data); //J
 		while(!queue.isEmpty()) {
 			MyNode<T> cNode = queue.removeFirst();
-			
-			ArrayList<MyNode<T>>  list = new ArrayList<MyNode<T>> (cNode.neighbourMap.keySet().stream().collect(Collectors.toList()));
-			
+
+			ArrayList<MyNode<T>>  list = new ArrayList<MyNode<T>> (cNode.neighbourMap.keySet());//.stream().collect(Collectors.toList()));
+
 			System.out.println(list.size() + " BIG array");
 			if (cNode.equals(last)) {
 				return true;
@@ -328,7 +416,7 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 			}
 
 			for (int i = 0; i < list.size(); i++ ) {
-						queue.addLast(list.get(i));
+				queue.addLast(list.get(i));
 				System.out.println(queue.size() + " Queue Size");
 				//			if ((boolean) queue.peekFirst().data)
 			}
@@ -337,83 +425,86 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 			}
 		}
 		return false;
-		//		System.out.println(path.size() + " Pree");
-		//		System.out.println(path.size() + " Post");
-		//		if(s.neighbourMap.keySet().isEmpty()) {
-		//			System.out.println(path.size() + " add to Queue");
-		//			queue.addAll(s.neighbourMap.keySet());
-		//			System.out.println(path.size() + " Post add to Queue");
-		//		}
-		//		else if(!s.equals(last)) {
-		//			path.remove(path.size()-1);
-		//		}
-		//		System.out.println(queue.size() + " Efter");
-		//		if(!queue.isEmpty()) {
-		//			System.out.println(queue.size() + " Rekursiv");
-		//			bfs(queue.peekFirst().data, last.data);
-		//		}
-		//
-		//		return path;	
-
-
-		//
-		//		//		MyNode<T> current = new MyNode<T>(start);
-		//		//		MyNode<T> last = new MyNode<T>(end);
-		//		//		//		”Set Node 1 as the start Node
-		//		//		queue.add(current); 
-		//		////		System.out.println(current+" current"); 
-		//		//		while (queue.size() != 0 && !current.equals(last)) 
-		//		//		{ 
-		//		//
-		//		//		//		Add this Node to the Queue
-		//		//		//		Add this Node to the visited set
-		//		//		current.visited =true;
-		//		//		
-		//		//		visited.add(current);
-		//		//		if (visited != null)
-		//		//			System.out.println(visited.size() + " visited"); 
-		//		//			
-		//		//		//		If this node is our goal node then return true, else add Node 2 and Node 3 to our Queue
-		//		//
-		//		//		//		Check Node 2 and if it isn’t add both Node 4 and Node 5 to our Queue. 
-		//		//		//		Take the next node from our Queue which should be Node 3 and check that.
-		//		//		
-		//		//		//		If Node 3 isn’t our goal node add Node 6 and Node 7 to our Queue.
-		//		//		//		Repeat until goal Node is found.”
-		//		//
-		//		//		// Mark the current node as visited and enqueue it 
-		//		//
-		//		//			// Dequeue a vertex from queue and print it 
-		//		//			current = queue.poll(); 
-		//		//			System.out.println(current+" current"); 
-		//		//
-		//		//			// Get all adjacent vertices of the dequeued vertex s 
-		//		//			// If a adjacent has not been visited, then mark it 
-		//		//			// visited and enqueue it 
-		//		//			ArrayList<T> list = current.getNeighbours();
-		//		//			for (T t : list) {
-		//		//				System.out.println(t+ " toString");
-		//		//			}
-		//		//			Iterator<T> i = current.getNeighbours().listIterator(); 
-		//		//			System.out.println(i+" i"); 
-		//		//			if(i.hasNext()) {
-		//		//				System.out.println(" hasNext"); 
-		//		//				
-		//		//			}
-		//		//			while (i.hasNext()) 
-		//		//			{ 
-		//		//				System.out.println(" WHUKE"); 
-		//		//				T n = i.next();
-		//		//				MyNode<T> tmp = new MyNode<T>(n);
-		//		//				if (!tmp.visited) 
-		//		//				{ 
-		//		//					tmp.visited = true; 
-		//		//					queue.add(tmp); 
-		//		//				} 
-		//		//			} 
-		//		//		} 
-		//
 	}
+	//		private boolean bfs(T start, T end ) { //J
+	//		System.out.println(path.size() + " Pree");
+	//		System.out.println(path.size() + " Post");
+	//		if(s.neighbourMap.keySet().isEmpty()) {
+	//			System.out.println(path.size() + " add to Queue");
+	//			queue.addAll(s.neighbourMap.keySet());
+	//			System.out.println(path.size() + " Post add to Queue");
+	//		}
+	//		else if(!s.equals(last)) {
+	//			path.remove(path.size()-1);
+	//		}
+	//		System.out.println(queue.size() + " Efter");
+	//		if(!queue.isEmpty()) {
+	//			System.out.println(queue.size() + " Rekursiv");
+	//			bfs(queue.peekFirst().data, last.data);
+	//		}
+	//
+	//		return path;	
+
+
+	//
+	//		//		MyNode<T> current = new MyNode<T>(start);
+	//		//		MyNode<T> last = new MyNode<T>(end);
+	//		//		//		”Set Node 1 as the start Node
+	//		//		queue.add(current); 
+	//		////		System.out.println(current+" current"); 
+	//		//		while (queue.size() != 0 && !current.equals(last)) 
+	//		//		{ 
+	//		//
+	//		//		//		Add this Node to the Queue
+	//		//		//		Add this Node to the visited set
+	//		//		current.visited =true;
+	//		//		
+	//		//		visited.add(current);
+	//		//		if (visited != null)
+	//		//			System.out.println(visited.size() + " visited"); 
+	//		//			
+	//		//		//		If this node is our goal node then return true, else add Node 2 and Node 3 to our Queue
+	//		//
+	//		//		//		Check Node 2 and if it isn’t add both Node 4 and Node 5 to our Queue. 
+	//		//		//		Take the next node from our Queue which should be Node 3 and check that.
+	//		//		
+	//		//		//		If Node 3 isn’t our goal node add Node 6 and Node 7 to our Queue.
+	//		//		//		Repeat until goal Node is found.”
+	//		//
+	//		//		// Mark the current node as visited and enqueue it 
+	//		//
+	//		//			// Dequeue a vertex from queue and print it 
+	//		//			current = queue.poll(); 
+	//		//			System.out.println(current+" current"); 
+	//		//
+	//		//			// Get all adjacent vertices of the dequeued vertex s 
+	//		//			// If a adjacent has not been visited, then mark it 
+	//		//			// visited and enqueue it 
+	//		//			ArrayList<T> list = current.getNeighbours();
+	//		//			for (T t : list) {
+	//		//				System.out.println(t+ " toString");
+	//		//			}
+	//		//			Iterator<T> i = current.getNeighbours().listIterator(); 
+	//		//			System.out.println(i+" i"); 
+	//		//			if(i.hasNext()) {
+	//		//				System.out.println(" hasNext"); 
+	//		//				
+	//		//			}
+	//		//			while (i.hasNext()) 
+	//		//			{ 
+	//		//				System.out.println(" WHUKE"); 
+	//		//				T n = i.next();
+	//		//				MyNode<T> tmp = new MyNode<T>(n);
+	//		//				if (!tmp.visited) 
+	//		//				{ 
+	//		//					tmp.visited = true; 
+	//		//					queue.add(tmp); 
+	//		//				} 
+	//		//			} 
+	//		//		} 
+	//
+	//	}
+
 	@Override
 	public UndirectedGraph<T> minimumSpanningTree() {
 		// TODO Auto-generated method stub
